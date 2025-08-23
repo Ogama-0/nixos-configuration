b745182e710055a73b10fad0edbeaac54e1d4356
Oscar <oscar.cornut@gmail.com>
2025-08-23 20:17:26 +0200

    feat(serv): micro update n3

--- modules/nixosconf/ssh.nix
+++ modules/nixosconf/ssh.nix
@@ -9,6 +9,12 @@
     };
   };
 
+  programs.ssh.knownHosts.ogama = {
+
+    publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaKjKo1kaGVYdA5U6kvrjhDj1T3tp04CmiZE3YmA7id ogama@nixos";
+  };
+
   networking.firewall.allowedTCPPorts = [ 22 ];
 }
 
