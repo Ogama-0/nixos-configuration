f5d825abc4fe8af48fe1671002a2edd9dcb48704
Oscar <oscar.cornut@gmail.com>
2025-08-23 20:22:22 +0200

    feat(serv): micro update n4

--- serveur/configuration.nix
+++ serveur/configuration.nix
@@ -23,6 +23,9 @@
   users.users.ogama_serv = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaKjKo1kaGVYdA5U6kvrjhDj1T3tp04CmiZE3YmA7id ogama@nixos"
+    ];
   };
 
   environment.systemPackages = with pkgs; [ nano alsa-utils ];
