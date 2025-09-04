3886bf83435576b17a76100e97327cca4d8f746f
Oscar <oscar.cornut@gmail.com>
2025-09-04 17:17:50 +0200

    feat(epita): edit font.nix path

--- epita/epita.nix
+++ epita/epita.nix
@@ -3,8 +3,8 @@
     ../modules/home-manager/git.nix
     ../modules/home-manager/helix.nix
     ../modules/home-manager/sh.nix
-    ../modules/home-manager/display/fonts.nix
-    .../modules/home-manager/display/i3.nix
+    ../modules/home-manager/display/font.nix
+    # ../modules/home-manager/display/i3.nix
     ../modules/home-manager/joplin-desktop.nix
   ];
   # General informations
