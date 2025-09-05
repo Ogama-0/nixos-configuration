e826b015fbc5b0d6e62a18dd3b0b85f5b0d91689
Oscar <oscar.cornut@gmail.com>
2025-09-05 08:31:08 +0200

    feat(epita): add i3bar import to epita configuration

--- host/epita/epita.nix
+++ host/epita/epita.nix
@@ -5,6 +5,7 @@
     ../../modules/home-manager/sh.nix
     ../../modules/home-manager/display/font.nix
     ../../modules/home-manager/display/i3.nix
+    ../../modules/home-manager/display/i3barbar.nix
     ../../modules/home-manager/joplin-desktop.nix
     ../../modules/home-manager/kitty.nix
     ../../modules/home-manager/display/gtk.nix
