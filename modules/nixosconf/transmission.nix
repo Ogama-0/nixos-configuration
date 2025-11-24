1509dd6b7cd436e86f3ececfb7cbed0bfab765f0
ogama_serv <oscar.cornut@gmail.com>
2025-11-24 21:42:28 +0100

    feat(serv): tofix: add transmission insteed of qbtorrent

--- modules/nixosconf/transmission.nix
+++ modules/nixosconf/transmission.nix
@@ -1,11 +1,16 @@
-{ ... }: {
+{ cfg, ... }:
+let Jellyfin_torrent_path = "${cfg.HDD_path}/appdata/jellyfin/FILM/TORRENT";
+in {
   services.transmission = {
     enable = true; # Enable transmission daemon
     openRPCPort = true; # Open firewall for RPC
+    openFirewall = true;
+    openPeerPorts = true;
     settings = { # Override default settings
       rpc-bind-address = "0.0.0.0"; # Bind to own IP
-      rpc-whitelist =
-        "127.0.0.1,10.0.0.1"; # Whitelist your remote machine (10.0.0.1 in this example)
+      peer-port = 51413;
+      rpc-whitelist = "127.0.0.1,100.*.*.*,192.168.1.*"; # Tails scale hosts
+      download-dir = Jellyfin_torrent_path;
     };
   };
 }
