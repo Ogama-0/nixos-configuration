83ddeb341f295b0b84bdf50890cfa8a47b71166a
ogama_serv <oscar.cornut@gmail.com>
2026-01-05 14:59:51 +0100

    fix(serv): fix qbttorrent

--- modules/nixosconf/qbittorrent.nix
+++ modules/nixosconf/qbittorrent.nix
@@ -3,6 +3,8 @@ let
   qb_root = "/home/${cfg.user}/.qbittorrent";
   save_path = "${qb_root}/torrent_file";
   hdd_file_path = "${cfg.HDD_app}/qbittorrent";
+  torrent_file_path = "${hdd_file_path}/torrents";
+  incomplet_file_path = "${hdd_file_path}/logs";
   movie_file_path = "${cfg.HDD_app}/jellyfin/FILM";
 in {
   services.qbittorrent = {
@@ -12,6 +14,8 @@ in {
   systemd.tmpfiles.rules = [
     "d ${qb_root}                0755 qbittorrent qbittorrent -"
     "d ${save_path}              0755 qbittorrent qbittorrent -"
-    "d ${hdd_file_path}              0755 qbittorrent qbittorrent -"
+    "d ${hdd_file_path}          0755 qbittorrent qbittorrent -"
+    "d ${torrent_file_path}      0755 qbittorrent qbittorrent -"
+    "d ${incomplet_file_path}    0755 qbittorrent qbittorrent -"
   ];
 }
